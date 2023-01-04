075af26c497d52cea015a6fb632acc3ba2e99a28
Paul Negoescu <paul.negoescu@nagarro.com>
2023-01-04 17:32:51 +0200

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+//Cum luam input de la utilizatori?
+
+//Cum afisam in consola orice?
+
+//Media a doua numere
+
+//Sa se calculeze media unui student constand din 3 numere
+//si sa se afiseze in consola "Trece" sau "Pica" in functie de medie, 5 e nota de trecere minima.
+
+//Maximul a doua numere
+
+//Maximul a trei numere
+
+//Sa se afiseze toate numerele de la 1 la 20
+
+//Sa se afiseze toate numerele pare de la 1 la 20 inclusiv
+
+//Maximul a oricate numere
+
+//Media a oricate numere
+
+//Sa se inverseze un sting: "Paul" -> "luaP"
+
+//Sa se determine daca un string este un palindrom: 'tacocat', '1234321'
+
+//Calculul factorialului, 5! = 1 * 2 * 3 * 4 * 5
